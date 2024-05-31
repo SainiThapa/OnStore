f3d201ffcdb7c35bea068958006f8a95c1dcbf34
SainiThapa <magarprisu4747@gmail.com>
2024-05-31 14:28:03 +0545

    .

--- Products/models.py
+++ Products/models.py
@@ -1,7 +1,7 @@
 import uuid
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+from account.models import Client
 
 # Create your models here.
     
@@ -19,19 +19,34 @@ class SubCatalog(models.Model):
         return f"{self.catalog} - {self.subcatalog_name}"
 
 class Product(models.Model):
-    category = models.ForeignKey(SubCatalog,verbose_name="Category",on_delete=models.CASCADE)
-    product_name = models.CharField(max_length=128)
-    product_img=models.ImageField()
-    product_id =  models.UUIDField(primary_key=True, default=uuid.uuid4)
-    product_price =  models.FloatField()
-    product_rating=models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    category = models.ForeignKey(SubCatalog,verbose_name="SubCatalog",on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    img=models.ImageField()
+    id =  models.UUIDField(primary_key=True, default=uuid.uuid4)
+    price =  models.FloatField()
+    rating=models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     added_to_cart = models.BooleanField(default=False)
     def __str__(self) -> str:
-        return (f"{self.product_name} - {self.product_id}")
+        return (f"{self.name} - {self.id}")
 
 class Cart(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True,null=True)
+    updated_at=models.DateTimeField(auto_now=True,null=True)
+    customer=models.OneToOneField(Client,on_delete=models.CASCADE,primary_key=True)
+
+    def __str__(self) -> str:
+        return f"Cart {self.id} created at {self.created_at}"
+
+    def total_price(self):
+        return sum(item.total_price() for item in self.cartitem_set.all())
+
+class CartItem(models.Model):
+    cart=models.ForeignKey(Cart,on_delete=models.CASCADE)
     product=models.ForeignKey(Product,on_delete=models.CASCADE)
-    cart_quantity=models.IntegerField()
-    total_price=models.FloatField()
+    quantity=models.IntegerField()
+
     def __str__(self) -> str:
         return f"{self.product} - {self.total_price}"
+    
+    def total_price(self):
+        return self.quantity*self.product.price
--- Products/views.py
+++ Products/views.py
@@ -3,10 +3,5 @@ from django.shortcuts import render
 from Products.models import Catalog, SubCatalog
 
 # Create your views here.
-
-def delete(request):
-    SubCatalog.objects.all().delete()
-    SubCatalog.save()
-    Catalog.objects.all().delete()
-    Catalog.save()
-    return render(request, "delete.html")
+def home(request):
+    return (request,"index.html")
